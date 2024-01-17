3d4ab864a09f6c443c644087c0965bca6524c4d8
Noris69 <cheikh.noris69@gmail.com>
2024-01-17 15:07:46 +0100

    first commit

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ app.get('/ambulances', (req, res)=>{
     res.status(200).json(ambulances)
 })
 
+app.get('/ambulances/:id', (req, res)=>{
+    const id = parseInt(req.params.id)
+    const ambulance = ambulances.find(ambulance => ambulance.id === id)
+    res.status(200).json(ambulance)
+})
 
 app.listen(8080, () => {console.log("Server started on 8080..")})
