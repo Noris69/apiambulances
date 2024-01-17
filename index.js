9fe5ff8a3b2e7f7b97f156ac546c0d00c4751925
Noris69 <cheikh.noris69@gmail.com>
2024-01-17 14:19:41 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const ambulances = require('./data/ambulances.json')
+
+app.get('/ambulances', (req, res)=>{
+    res.status(200).json(ambulances)
+})
+
+
+app.listen(8080, () => {console.log("Server started on 8080..")})
