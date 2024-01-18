aca1ebebaa7d2c59f51b087baa6bd91d19e5d17b
Noris69 <cheikh.noris69@gmail.com>
2024-01-18 10:03:12 +0100

    first commit

--- index.js
+++ index.js
@@ -23,4 +23,22 @@ app.get('/ambulances/:id', (req, res)=>{
     res.status(200).json(ambulance)
 })
 
+app.put('/ambulances/:id', (req, res)=>{
+    const id = parseInt(req.params.id)
+    let ambulance = ambulances.find(ambulance => ambulance.id === id);
+    ambulance.marque = req.body.marque
+    ambulance.modele = req.body.modele
+    ambulance.couleur = req.body.couleur
+
+    res.status(303).json(ambulance)
+})
+
+app.delete('/ambulances/:id', (req, res)=>{
+    const id = parseInt(req.params.id)
+    let ambulance = ambulances.find(ambulance => ambulance.id === id);
+    ambulances.splice(ambulances.indexOf(ambulance), 1);
+
+    res.status(200).json(ambulance)
+})
+
 app.listen(8080, () => {console.log("Server started on 8080..")})
